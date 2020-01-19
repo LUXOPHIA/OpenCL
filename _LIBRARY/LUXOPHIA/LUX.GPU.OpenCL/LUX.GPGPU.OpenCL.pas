4512662c2a59758d4fba53db5a2dcb8336e339a3
luxidea <contact@luxidea.net>
2020-01-19 16:23:43 +0900

    \{\$IF ([^ ]+) <> 0 \} → {$IF Declared( $1 ) }

--- _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPGPU.OpenCL.pas
+++ _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPGPU.OpenCL.pas
@@ -87,11 +87,11 @@ begin
        CL_IMAGE_FORMAT_NOT_SUPPORTED                : Result := 'IMAGE_FORMAT_NOT_SUPPORTED';
        CL_BUILD_PROGRAM_FAILURE                     : Result := 'BUILD_PROGRAM_FAILURE';
        CL_MAP_FAILURE                               : Result := 'MAP_FAILURE';
-{$IF CL_VERSION_1_1 <> 0 }
+{$IF Declared( CL_VERSION_1_1 ) }
        CL_MISALIGNED_SUB_BUFFER_OFFSET              : Result := 'MISALIGNED_SUB_BUFFER_OFFSET';
        CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST  : Result := 'EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST';
 {$ENDIF}
-{$IF CL_VERSION_1_2 <> 0 }
+{$IF Declared( CL_VERSION_1_2 ) }
        CL_COMPILE_PROGRAM_FAILURE                   : Result := 'COMPILE_PROGRAM_FAILURE';
        CL_LINKER_NOT_AVAILABLE                      : Result := 'LINKER_NOT_AVAILABLE';
        CL_LINK_PROGRAM_FAILURE                      : Result := 'LINK_PROGRAM_FAILURE';
@@ -132,20 +132,20 @@ begin
        CL_INVALID_BUFFER_SIZE                       : Result := 'INVALID_BUFFER_SIZE';
        CL_INVALID_MIP_LEVEL                         : Result := 'INVALID_MIP_LEVEL';
        CL_INVALID_GLOBAL_WORK_SIZE                  : Result := 'INVALID_GLOBAL_WORK_SIZE';
-{$IF CL_VERSION_1_1 <> 0 }
+{$IF Declared( CL_VERSION_1_1 ) }
        CL_INVALID_PROPERTY                          : Result := 'INVALID_PROPERTY';
 {$ENDIF}
-{$IF CL_VERSION_1_2 <> 0 }
+{$IF Declared( CL_VERSION_1_2 ) }
        CL_INVALID_IMAGE_DESCRIPTOR                  : Result := 'INVALID_IMAGE_DESCRIPTOR';
        CL_INVALID_COMPILER_OPTIONS                  : Result := 'INVALID_COMPILER_OPTIONS';
        CL_INVALID_LINKER_OPTIONS                    : Result := 'INVALID_LINKER_OPTIONS';
        CL_INVALID_DEVICE_PARTITION_COUNT            : Result := 'INVALID_DEVICE_PARTITION_COUNT';
 {$ENDIF}
-{$IF CL_VERSION_2_0 <> 0 }
+{$IF Declared( CL_VERSION_2_0 ) }
        CL_INVALID_PIPE_SIZE                         : Result := 'INVALID_PIPE_SIZE';
        CL_INVALID_DEVICE_QUEUE                      : Result := 'INVALID_DEVICE_QUEUE';
 {$ENDIF}
-{$IF CL_VERSION_2_2 <> 0 }
+{$IF Declared( CL_VERSION_2_2 ) }
        CL_INVALID_SPEC_ID                           : Result := 'INVALID_SPEC_ID';
        CL_MAX_SIZE_RESTRICTION_EXCEEDED             : Result := 'MAX_SIZE_RESTRICTION_EXCEEDED';
 {$ENDIF}
