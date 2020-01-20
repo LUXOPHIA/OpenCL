042c66cb6dc430dd725defb9a9b54e2033bbdc58
luxidea <contact@luxidea.net>
2020-01-21 03:16:42 +0900

    表示を改善。

--- Main.pas
+++ Main.pas
@@ -16,6 +16,8 @@ type
         Memo1: TMemo;
       TabItem2: TTabItem;
         Memo2: TMemo;
+    TabItem3: TTabItem;
+    Memo3: TMemo;
     procedure FormDestroy(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
@@ -24,8 +26,9 @@ type
     { public 宣言 }
     _Context :TCLContext;
     ///// メソッド
-    procedure ShowOPENCL;
-    procedure ShowContext;
+    procedure ShowPlatforms;
+    procedure ShowDevices;
+    procedure ShowContexts;
   end;
 
 var
@@ -35,71 +38,108 @@ implementation //###############################################################
 
 {$R *.fmx}
 
-
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
-procedure TForm1.ShowOPENCL;
+procedure TForm1.ShowPlatforms;
 var
-   PI, EI, DI :Integer;
+   PI, EI :Integer;
    P :TCLPlatform;
-   D :TCLDevice;
+   E :String;
 begin
      with Memo1.Lines do
      begin
-          Add( '・Platforms' );
-
           for PI := 0 to _OpenCL_.Platforms.Count-1 do
           begin
                P := _OpenCL_.Platforms[ PI ];
 
-               Add( '　・Platform[ ' + PI.ToString + ' ]：$' + Longint( P.ID ).ToHexString );
-
-               Add( '　　・Profile：' + P.Profile );
-               Add( '　　・Version：' + P.Version );
-               Add( '　　・Name   ：' + P.Name    );
-               Add( '　　・Vendor ：' + P.Vendor  );
-
-               Add( '　　・Extensions' );
-               for EI := 0 to P.Extensions.Count-1 do Add( '　　　・' + P.Extensions[ EI ] );
-
-               Add( '　　・Devices' );
-               for DI := 0 to P.Devices.Count-1 do
+               Add( '┃' );
+               Add( '┣・Platform[ ' + PI.ToString + ' ]<' + Longint( P.ID ).ToHexString + '>' );
+               Add( '┃　├・Profile   ：' + P.Profile );
+               Add( '┃　├・Version   ：' + P.Version );
+               Add( '┃　├・Name      ：' + P.Name    );
+               Add( '┃　├・Vendor    ：' + P.Vendor  );
+               Add( '┃　├・Extensions：' );
+               for EI := 0 to P.Extensions.Count-1 do
                begin
-                    D := P.Devices[ DI ];
+                    E := P.Extensions[ EI ];
 
-                    Add( '　　　・Device[ ' + DI.ToString + ' ]：$' + Longint( D.ID ).ToHexString );
-
-                    Add( '　　　　・DEVICE_TYPE     ：'  + IntTosTr( D.DEVICE_TYPE      ) );
-                    Add( '　　　　・DEVICE_VENDOR   ：'  +           D.DEVICE_VENDOR      );
-                    Add( '　　　　・DEVICE_VENDOR_ID：'  + IntToStr( D.DEVICE_VENDOR_ID ) );
-                    Add( '　　　　・DEVICE_VERSION  ：'  +           D.DEVICE_VERSION     );
-                    Add( '　　　　・DRIVER_VERSION  ：'  +           D.DRIVER_VERSION     );
+                    Add( '┃　│　 - ' + E );
                end;
           end;
+          Add( '' );
      end;
 end;
 
 //------------------------------------------------------------------------------
 
-procedure TForm1.ShowContext;
+procedure TForm1.ShowDevices;
 var
-   CI :Integer;
-   C :TCLCommand;
+   PI, DI :Integer;
+   P :TCLPlatform;
+   D :TCLDevice;
 begin
      with Memo2.Lines do
      begin
-          Add( '・Handle  ：$' + LongInt( _Context.Handle ).ToHexString );
-
-          Add( '・Commands：' );
-          for CI := 0 to _Context.Commands.Count-1 do
+          for PI := 0 to _OpenCL_.Platforms.Count-1 do
           begin
-               C := _Context.Commands[ CI ];
+               P := _OpenCL_.Platforms[ PI ];
 
-               Add( '　・Command[ ' + CI.ToString + ' ]' );
-               Add( '　　・Handle：$' + LongInt( C.Handle    ).ToHexString );
-               Add( '　　・Device：$' + LongInt( C.Device.ID ).ToHexString );
+               Add( '┃' );
+               Add( '┣・Platform[ ' + PI.ToString + ' ]<' + Longint( P.ID ).ToHexString + '>' );
+               Add( '┃　│' );
+               for DI := 0 to P.Devices.Count-1 do
+               begin
+                    D := P.Devices[ DI ];
+
+                    Add( '┃　│' );
+                    Add( '┃　┝・Device[ ' + DI.ToString + ' ]<' + Longint( D.ID ).ToHexString + '>' );
+                    Add( '┃　│　├・DEVICE_TYPE     ：'  + IntTosTr( D.DEVICE_TYPE      ) );
+                    Add( '┃　│　├・DEVICE_VENDOR   ：'  +           D.DEVICE_VENDOR      );
+                    Add( '┃　│　├・DEVICE_VENDOR_ID：'  + IntToStr( D.DEVICE_VENDOR_ID ) );
+                    Add( '┃　│　├・DEVICE_VERSION  ：'  +           D.DEVICE_VERSION     );
+                    Add( '┃　│　├・DRIVER_VERSION  ：'  +           D.DRIVER_VERSION     );
+               end;
           end;
+          Add( '' );
+     end;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TForm1.ShowContexts;
+var
+   PI, CI, QI :Integer;
+   P :TCLPlatform;
+   C :TCLCOntext;
+   Q :TCLCommand;
+begin
+     with Memo3.Lines do
+     begin
+          for PI := 0 to _OpenCL_.Platforms.Count-1 do
+          begin
+               P := _OpenCL_.Platforms[ PI ];
+
+               Add( '┃' );
+               Add( '┣・Platform[ ' + PI.ToString + ' ]<' + Longint( P.ID ).ToHexString + '>' );
+               for CI := 0 to P.Contexts.Count-1 do
+               begin
+                    C := P.Contexts[ CI ];
+
+                    Add( '┃　│' );
+                    Add( '┃　┝・Context[ ' + CI.ToString + ' ]<' + LongInt( C.Handle ).ToHexString + '>' );
+                    for QI := 0 to C.Commands.Count-1 do
+                    begin
+                         Q := C.Commands[ QI ];
+
+                         Add( '┃　│　│' );
+                         Add( '┃　│　├・Command[ ' + QI.ToString + ' ]<' + LongInt( Q.Handle ).ToHexString + '>'
+                            + ' → Device<' + LongInt( Q.Device.ID ).ToHexString + '>' );
+                    end;
+               end;
+          end;
+          Add( '' );
      end;
 end;
 
@@ -107,13 +147,14 @@ end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-     ShowOPENCL;
+     ShowPlatforms;
+     ShowDevices;
 
      _Context := TCLContext.Create( _OpenCL_.Platforms[ 0 ] );
 
      _Context.Add( _OpenCL_.Platforms[ 0 ].Devices[ 0 ] );
 
-     ShowContext;
+     ShowContexts;
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
