92d3bdb487721de58dc7f1769f7141c2b01d6240
luxidea <contact@luxidea.net>
2020-01-21 18:32:38 +0900

    TCLVersion 列挙型を追加。 ※ Record Helper を使うと他ユニットの定数が認識できなくなる。

--- _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPU.OpenCL.root.pas
+++ _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPU.OpenCL.root.pas
@@ -7,9 +7,27 @@ uses cl_version,
      cl,
      LUX.Code.C;
 
-//type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLVersion
+
+     TCLVersion = ( None  =   0,    // 未定義
+                    CL100 = 100,    // OpenCL 1.0
+                    CL110 = 110,    // OpenCL 1.1
+                    CL120 = 120,    // OpenCL 1.2
+                    CL200 = 200,    // OpenCL 2.0
+                    CL210 = 210,    // OpenCL 2.1
+                    CL220 = 220 );  // OpenCL 2.2
+
+     HCLVersion = record helper for TCLVersion
+     {private}
+     {public}
+       class function From( const Value_:String ) :TCLVersion; static;
+       ///// メソッド
+       function ToString :String;
+     end;
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
 
@@ -26,6 +44,40 @@ procedure AssertCL( const Error_:T_cl_int );
 implementation //############################################################### ■
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLVersion
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+class function HCLVersion.From( const Value_:String ) :TCLVersion;
+begin
+     if Value_ = '1.0' then Result := TCLVersion.CL100
+                       else
+     if Value_ = '1.1' then Result := TCLVersion.CL110
+                       else
+     if Value_ = '1.2' then Result := TCLVersion.CL120
+                       else
+     if Value_ = '2.0' then Result := TCLVersion.CL200
+                       else
+     if Value_ = '2.1' then Result := TCLVersion.CL210
+                       else
+     if Value_ = '2.2' then Result := TCLVersion.CL220
+                       else Result := TCLVersion.None;
+end;
+
+function HCLVersion.ToString :String;
+begin
+     case Self of
+       TCLVersion.CL100: Result := '1.0';
+       TCLVersion.CL110: Result := '1.1';
+       TCLVersion.CL120: Result := '1.2';
+       TCLVersion.CL200: Result := '2.0';
+       TCLVersion.CL210: Result := '2.1';
+       TCLVersion.CL220: Result := '2.2';
+     end;
+end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
 
@@ -47,11 +99,11 @@ begin
        CL_IMAGE_FORMAT_NOT_SUPPORTED                : Result := 'IMAGE_FORMAT_NOT_SUPPORTED';
        CL_BUILD_PROGRAM_FAILURE                     : Result := 'BUILD_PROGRAM_FAILURE';
        CL_MAP_FAILURE                               : Result := 'MAP_FAILURE';
-{$IF CL_VERSION_1_1 <> 0 }
+{$IF cl_version.CL_VERSION_1_1 <> 0 }                                           { TODO : Record Helper を使うと他ユニットの定数が認識できなくなる。 }
        CL_MISALIGNED_SUB_BUFFER_OFFSET              : Result := 'MISALIGNED_SUB_BUFFER_OFFSET';
-       CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST  : Result := 'EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST';
+       CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST : Result := 'EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST';
 {$ENDIF}
-{$IF CL_VERSION_1_2 <> 0 }
+{$IF cl_version.CL_VERSION_1_2 <> 0 }
        CL_COMPILE_PROGRAM_FAILURE                   : Result := 'COMPILE_PROGRAM_FAILURE';
        CL_LINKER_NOT_AVAILABLE                      : Result := 'LINKER_NOT_AVAILABLE';
        CL_LINK_PROGRAM_FAILURE                      : Result := 'LINK_PROGRAM_FAILURE';
@@ -92,20 +144,20 @@ begin
        CL_INVALID_BUFFER_SIZE                       : Result := 'INVALID_BUFFER_SIZE';
        CL_INVALID_MIP_LEVEL                         : Result := 'INVALID_MIP_LEVEL';
        CL_INVALID_GLOBAL_WORK_SIZE                  : Result := 'INVALID_GLOBAL_WORK_SIZE';
-{$IF CL_VERSION_1_1 <> 0 }
+{$IF cl_version.CL_VERSION_1_1 <> 0 }
        CL_INVALID_PROPERTY                          : Result := 'INVALID_PROPERTY';
 {$ENDIF}
-{$IF CL_VERSION_1_2 <> 0 }
+{$IF cl_version.CL_VERSION_1_2 <> 0 }
        CL_INVALID_IMAGE_DESCRIPTOR                  : Result := 'INVALID_IMAGE_DESCRIPTOR';
        CL_INVALID_COMPILER_OPTIONS                  : Result := 'INVALID_COMPILER_OPTIONS';
        CL_INVALID_LINKER_OPTIONS                    : Result := 'INVALID_LINKER_OPTIONS';
        CL_INVALID_DEVICE_PARTITION_COUNT            : Result := 'INVALID_DEVICE_PARTITION_COUNT';
 {$ENDIF}
-{$IF CL_VERSION_2_0 <> 0 }
+{$IF cl_version.CL_VERSION_2_0 <> 0 }
        CL_INVALID_PIPE_SIZE                         : Result := 'INVALID_PIPE_SIZE';
        CL_INVALID_DEVICE_QUEUE                      : Result := 'INVALID_DEVICE_QUEUE';
 {$ENDIF}
-{$IF CL_VERSION_2_2 <> 0 }
+{$IF cl_version.CL_VERSION_2_2 <> 0 }
        CL_INVALID_SPEC_ID                           : Result := 'INVALID_SPEC_ID';
        CL_MAX_SIZE_RESTRICTION_EXCEEDED             : Result := 'MAX_SIZE_RESTRICTION_EXCEEDED';
 {$ENDIF}
