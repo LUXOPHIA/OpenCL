b4038bfb668cfcc6d4d284c0230df5bf5e56efcc
luxidea <contact@luxidea.net>
2021-04-14 02:01:07 +0900

    no message

--- Main.pas
+++ Main.pas
@@ -5,7 +5,7 @@ interface //####################################################################
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, FMX.TabControl,
+  FMX.Memo.Types, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, FMX.TabControl,
   cl_version,
   cl_platform,
   cl,
