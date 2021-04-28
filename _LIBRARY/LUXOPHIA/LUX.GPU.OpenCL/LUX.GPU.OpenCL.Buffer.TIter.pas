e380e8a978390add0be6f83167c13c63a405f1e6
luxidea <contact@luxidea.net>
2021-04-29 04:37:23 +0900

    TCLBufferIter .Command → Comman プロパティへ改名。

--- _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPU.OpenCL.Buffer.TIter.pas
+++ _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPU.OpenCL.Buffer.TIter.pas
@@ -15,29 +15,29 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLBufferIter<_TContext_,_TDevice_,_TValue_>
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLBufferIter<TCLContex_,TCLDevice_,TValue_>
 
-     TCLBufferIter<_TContext_,_TDevice_:class;_TValue_:record> = class
+     TCLBufferIter<TCLContex_,TCLDevice_:class;TValue_:record> = class
      private
-       type TCLComman = TCLComman<_TContext_,_TDevice_>;
-       type TCLBuffer = TCLBuffer<_TContext_,_TValue_>;
-       type PValue    = ^_TValue_;
+       type TCLComman = TCLComman<TCLContex_,TCLDevice_>;
+            TCLBuffer = TCLBuffer<TCLContex_,TValue_>;
+            PValue    = ^TValue_;
      private
-       _Command :TCLComman;
-       _Buffer  :TCLBuffer;
-       _Mode    :T_cl_map_flags;
-       _Head    :PValue;
+       _Comman :TCLComman;
+       _Buffer :TCLBuffer;
+       _Mode   :T_cl_map_flags;
+       _Head   :PValue;
        ///// アクセス
-       function GetValues( const I_:Integer ) :_TValue_;
-       procedure SetValues( const I_:Integer; const Values_:_TValue_ );
+       function GetValues( const I_:Integer ) :TValue_;
+       procedure SetValues( const I_:Integer; const Values_:TValue_ );
      public
-       constructor Create( const Command_:TCLComman; const Buffer_:TCLBuffer; const Mode_:T_cl_map_flags = CL_MAP_READ or CL_MAP_WRITE );
+       constructor Create( const Comman_:TCLComman; const Buffer_:TCLBuffer; const Mode_:T_cl_map_flags = CL_MAP_READ or CL_MAP_WRITE );
        destructor Destroy; override;
        ///// プロパティ
-       property Command                    :TCLComman      read   _Command;
-       property Buffer                     :TCLBuffer      read   _Buffer;
-       property Mode                       :T_cl_map_flags read   _Mode;
-       property Values[ const I_:Integer ] :_TValue_       read GetValues write SetValues;
+       property Comman                     :TCLComman      read   _Comman                ;
+       property Buffer                     :TCLBuffer      read   _Buffer                ;
+       property Mode                       :T_cl_map_flags read   _Mode                  ;
+       property Values[ const I_:Integer ] :TValue_        read GetValues write SetValues;
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
@@ -54,20 +54,20 @@ uses LUX.GPU.OpenCL;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLBufferIter<_TContext_,_TDevice_,_TValue_>
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLBufferIter<TCLContex_,TCLDevice_,TValue_>
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
 
 /////////////////////////////////////////////////////////////////////// アクセス
 
-function TCLBufferIter<_TContext_,_TDevice_,_TValue_>.GetValues( const I_:Integer ) :_TValue_;
+function TCLBufferIter<TCLContex_,TCLDevice_,TValue_>.GetValues( const I_:Integer ) :TValue_;
 var
    P :PValue;
 begin
      P := _Head;  Inc( P, I_ );  Result := P^;
 end;
 
-procedure TCLBufferIter<_TContext_,_TDevice_,_TValue_>.SetValues( const I_:Integer; const Values_:_TValue_ );
+procedure TCLBufferIter<TCLContex_,TCLDevice_,TValue_>.SetValues( const I_:Integer; const Values_:TValue_ );
 var
    P :PValue;
 begin
@@ -76,24 +76,24 @@ end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
-constructor TCLBufferIter<_TContext_,_TDevice_,_TValue_>.Create( const Command_:TCLComman; const Buffer_:TCLBuffer; const Mode_:T_cl_map_flags = CL_MAP_READ or CL_MAP_WRITE );
+constructor TCLBufferIter<TCLContex_,TCLDevice_,TValue_>.Create( const Comman_:TCLComman; const Buffer_:TCLBuffer; const Mode_:T_cl_map_flags = CL_MAP_READ or CL_MAP_WRITE );
 var
    E :T_cl_int;
 begin
      inherited Create;
 
-     _Command := Command_;
-     _Buffer  := Buffer_;
-     _Mode    := Mode_;
+     _Comman := Comman_;
+     _Buffer := Buffer_;
+     _Mode   := Mode_;
 
-     _Head := clEnqueueMapBuffer( Command_.Handle, Buffer_.Handle, CL_TRUE, Mode_, 0, Buffer_.Size, 0, nil, nil, @E );
+     _Head := clEnqueueMapBuffer( Comman_.Handle, Buffer_.Handle, CL_TRUE, Mode_, 0, Buffer_.Size, 0, nil, nil, @E );
 
      AssertCL( E );
 end;
 
-destructor TCLBufferIter<_TContext_,_TDevice_,_TValue_>.Destroy;
+destructor TCLBufferIter<TCLContex_,TCLDevice_,TValue_>.Destroy;
 begin
-     AssertCL( clEnqueueUnmapMemObject( _Command.Handle, _Buffer.Handle, _Head, 0, nil, nil ) );
+     AssertCL( clEnqueueUnmapMemObject( _Comman.Handle, _Buffer.Handle, _Head, 0, nil, nil ) );
 
      inherited;
 end;
@@ -101,11 +101,5 @@ end;
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
-
-//############################################################################## □
-
-initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
-
-finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
 
 end. //######################################################################### ■
