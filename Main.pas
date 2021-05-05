5dad6e5a795c3ee9295330c2a836d625cb6fc110
luxidea <contact@luxidea.net>
2021-05-06 02:40:47 +0900

    no message

--- Main.pas
+++ Main.pas
@@ -162,9 +162,9 @@ begin
 
      ///// カーネル
      _Kernel := TCLKernel.Create( _Progra, 'Main', _Queuer );                   // 生成
-     _Kernel.Argumes.Add( _BufferA );                                           // バッファの登録
-     _Kernel.Argumes.Add( _BufferB );                                           // バッファの登録
-     _Kernel.Argumes.Add( _BufferC );                                           // バッファの登録
+     _Kernel.Argumes.Add( 'A', _BufferA );                                      // バッファの登録
+     _Kernel.Argumes.Add( 'B', _BufferB );                                      // バッファの登録
+     _Kernel.Argumes.Add( 'C', _BufferC );                                      // バッファの登録
      _Kernel.GlobWorkSize := [ 10 ];                                            // ループ回数の設定
 
      //////////
