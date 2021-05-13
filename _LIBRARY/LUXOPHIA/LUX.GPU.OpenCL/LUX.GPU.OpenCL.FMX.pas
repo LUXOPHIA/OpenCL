c558abe16ae49da55d9f9159c6a0d77081ce109c
luxidea <contact@luxidea.net>
2021-05-14 04:25:41 +0900

    TCLDevIma1DxBGRAxUInt8 クラスを追加。 TCLHosIma1DxBGRAxUInt8 TCLDevIma1DxRGBAxSFlo32 TCLHosIma1DxRGBAxSFlo32 TCLDevIma3DxBGRAxUInt8 TCLHosIma3DxBGRAxUInt8 TCLDevIma3DxRGBAxSFlo32 TCLHosIma3DxRGBAxSFlo32

--- _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPU.OpenCL.FMX.pas
+++ _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPU.OpenCL.FMX.pas
@@ -9,11 +9,23 @@ uses LUX.GPU.OpenCL,
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
 
+     TCLDevIma1DxBGRAxUInt8 = TCLDevIma1DxBGRAxUInt8<TCLContex,TCLPlatfo>;
+     TCLHosIma1DxBGRAxUInt8 = TCLHosIma1DxBGRAxUInt8<TCLContex,TCLPlatfo>;
+
+     TCLDevIma1DxRGBAxSFlo32 = TCLDevIma1DxRGBAxSFlo32<TCLContex,TCLPlatfo>;
+     TCLHosIma1DxRGBAxSFlo32 = TCLHosIma1DxRGBAxSFlo32<TCLContex,TCLPlatfo>;
+
      TCLDevIma2DxBGRAxUInt8 = TCLDevIma2DxBGRAxUInt8<TCLContex,TCLPlatfo>;
      TCLHosIma2DxBGRAxUInt8 = TCLHosIma2DxBGRAxUInt8<TCLContex,TCLPlatfo>;
 
      TCLDevIma2DxRGBAxSFlo32 = TCLDevIma2DxRGBAxSFlo32<TCLContex,TCLPlatfo>;
      TCLHosIma2DxRGBAxSFlo32 = TCLHosIma2DxRGBAxSFlo32<TCLContex,TCLPlatfo>;
+
+     TCLDevIma3DxBGRAxUInt8 = TCLDevIma3DxBGRAxUInt8<TCLContex,TCLPlatfo>;
+     TCLHosIma3DxBGRAxUInt8 = TCLHosIma3DxBGRAxUInt8<TCLContex,TCLPlatfo>;
+
+     TCLDevIma3DxRGBAxSFlo32 = TCLDevIma3DxRGBAxSFlo32<TCLContex,TCLPlatfo>;
+     TCLHosIma3DxRGBAxSFlo32 = TCLHosIma3DxRGBAxSFlo32<TCLContex,TCLPlatfo>;
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
