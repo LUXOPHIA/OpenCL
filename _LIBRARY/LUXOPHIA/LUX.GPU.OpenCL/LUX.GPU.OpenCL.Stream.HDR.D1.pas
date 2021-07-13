4bfeeddaf4048d501920ce3707fe897ef6c8c83d
luxidea <contact@luxidea.net>
2021-07-14 07:11:07 +0900

    TCLStream2DxRGBAxSFlo32_HDR クラスを追加。 LUX.GPU.OpenCL.Stream.HDR.D* ユニットを追加。

---
+++ _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPU.OpenCL.Stream.HDR.D1.pas
@@ -0,0 +1,97 @@
+﻿unit LUX.GPU.OpenCL.Stream.HDR.D1;
+
+interface //#################################################################### ■
+
+uses LUX.Color, LUX.Color.Grid.D2.Preset,
+     LUX.GPU.OpenCL;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     TCLStream1D_HDR<TCLImager_:class> = class;
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLStream1D_HDR<TCLImager_>
+
+     ICLStream1D_HDR<TCLImager_:class> = interface
+     ['{BE5886F8-7F3C-4460-829B-9756F3808B6E}']
+     {protected}
+       function GetImage :TCLImager_;
+       procedure SetImage( const Imager_:TCLImager_ );
+     {public}
+       ///// プロパティ
+       property Imager :TCLImager_ read GetImage write SetImage;
+       ///// メソッド
+       procedure CopyFrom( const Grid_:TCellColorGrid2D_TByteRGBE );
+       procedure LoadFromFile( const FileName_:String );
+     end;
+
+     TCLStream1D_HDR<TCLImager_:class> = class( TInterfacedObject, ICLStream1D_HDR<TCLImager_> )
+     private
+     protected
+       _Imager :TCLImager_;
+       ///// アクセス
+       function GetImage :TCLImager_;
+       procedure SetImage( const Imager_:TCLImager_ );
+     public
+       constructor Create; overload;
+       constructor Create( const Imager_:TCLImager_ ); overload;
+       ///// プロパティ
+       property Imager :TCLImager_ read GetImage write SetImage;
+       ///// メソッド
+       procedure CopyFrom( const Grid_:TCellColorGrid2D_TByteRGBE ); overload; virtual; abstract;
+       procedure LoadFromFile( const FileName_:String ); virtual; abstract;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+uses LUX.Color.Format.HDR;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLStream1D_HDR<TCLImager_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TCLStream1D_HDR<TCLImager_>.GetImage :TCLImager_;
+begin
+     Result := _Imager;
+end;
+
+procedure TCLStream1D_HDR<TCLImager_>.SetImage( const Imager_:TCLImager_ );
+begin
+     _Imager := Imager_;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TCLStream1D_HDR<TCLImager_>.Create;
+begin
+     inherited;
+
+end;
+
+constructor TCLStream1D_HDR<TCLImager_>.Create( const Imager_:TCLImager_ );
+begin
+     Create;
+
+     Imager := Imager_;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+end. //######################################################################### ■
---
+++ _LIBRARY/LUXOPHIA/LUX.GPU.OpenCL/LUX.GPU.OpenCL.Stream.HDR.D3.pas
@@ -0,0 +1,104 @@
+﻿unit LUX.GPU.OpenCL.Stream.HDR.D3;
+
+interface //#################################################################### ■
+
+uses LUX.Color, LUX.Color.Grid.D2.Preset,
+     LUX.GPU.OpenCL,
+     LUX.GPU.OpenCL.Stream.HDR.D2;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     TCLStream3D_HDR<TCLImager_:class> = class;
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLStream3D_HDR<TCLImager_>
+
+     ICLStream3D_HDR<TCLImager_:class> = interface( ICLStream2D_HDR<TCLImager_> )
+     ['{519564A2-6DEB-407F-B078-5147AD141A1A}']
+     {protected}
+     {public}
+       ///// メソッド
+       procedure CopyFrom( const Z_:Integer; const Grider_:TCellColorGrid2D_TByteRGBE );
+     end;
+
+     TCLStream3D_HDR<TCLImager_:class> = class( TCLStream2D_HDR<TCLImager_>, ICLStream3D_HDR<TCLImager_> )
+     private
+     protected
+     public
+       ///// メソッド
+       procedure CopyFrom( const Z_:Integer; const Grider_:TCellColorGrid2D_TByteRGBE ); overload; virtual; abstract;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLStream3DxRGBAxSFlo32_HDR
+
+     ICLStream3DxRGBAxSFlo32_HDR = ICLStream3D_HDR<TCLImager3DxRGBAxSFlo32>;
+
+     TCLStream3DxRGBAxSFlo32_HDR = class( TCLStream3D_HDR<TCLImager3DxRGBAxSFlo32>, ICLStream3DxRGBAxSFlo32_HDR )
+     private
+     protected
+     public
+       ///// メソッド
+       procedure CopyFrom( const Z_:Integer; const Grider_:TCellColorGrid2D_TByteRGBE ); override;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+uses System.Threading;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLStream3D_HDR<TCLImager_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCLStream3DxRGBAxSFlo32_HDR
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TCLStream3DxRGBAxSFlo32_HDR.CopyFrom( const Z_:Integer; const Grider_:TCellColorGrid2D_TByteRGBE );
+begin
+     with _Imager do
+     begin
+          CountX := Grider_.CellsX;
+          CountY := Grider_.CellsY;
+
+          Data.Map;
+
+          TParallel.For( 0, CountY-1, procedure( Y:Integer )
+          var
+             X :Integer;
+          begin
+               for X := 0 to CountX-1 do
+               begin
+                    Data[ X, Y, Z_ ] := TSingleRGBA( TSingleRGB( Grider_[ X, Y ] ) );
+               end;
+          end );
+
+          Data.Unmap;
+     end;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+end. //######################################################################### ■
